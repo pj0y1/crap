3edbbff19a2008280f998d75ba57dfa610e69702
go4math <54497693+go4math@users.noreply.github.com>
2023-03-11 11:31:31 +0800

    add version 0.8.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="crap",
-    version="0.7.0",
+    version="0.8.0",
     author="pj0y1",
     author_email="pj0y1@example.com",
     description="Learn to Pack Python Module",
