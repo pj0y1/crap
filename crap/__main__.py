177158de98de54f40c677d92433745f0f83d5288
go4math <54497693+go4math@users.noreply.github.com>
2023-03-11 11:00:04 +0800

    initial commit

---
+++ crap/__main__.py
@@ -0,0 +1,5 @@
+import crap
+
+
+if __name__ == '__main__':
+    print(crap.__version__)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages 
+
+
+setup(
+    name="crap",
+    version="0.6.0",
+    author="pj0y1",
+    author_email="pj0y1@example.com",
+    description="Learn to Pack Python Module",
+    url="https://pj0y1.github.io/", 
+    packages=find_packages(),
+)
